0623cbb6408769738ba16e90c4682af288674dac
Waidazzas <waidazzas@gmail.com>
2022-12-11 14:43:49 +0200

    Tuta HomeWork "LearningArray"

---
+++ LearningArrays/LearningArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = new string[] { "Vaidas", "Laurynas", "Zivile", "Lukrecija", "Grantas" };
+            Console.WriteLine("The names from array are:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Names[i]);
+            }
+            Console.WriteLine();
+
+            int[] Number = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            Array.Reverse(Number);
+            Console.Write("The array by descending order are: ");
+            foreach (var descending in Number)
+            {
+                Console.Write(descending + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
